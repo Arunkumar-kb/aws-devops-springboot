0d015b5f389f17c9076a459a10d52ebf287f5e49
Arunkumar <arunbabulal077@gmail.com>
2026-02-07 13:34:24 +0530

    Added controller-01

---
+++ src/main/java/com/aws/devops/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.aws.devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ProductController {
+
+    @GetMapping
+    public String welcome() {
+        return "welcome to devops learning..";
+    }
+}
